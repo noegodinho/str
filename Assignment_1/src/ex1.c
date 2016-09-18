4bf760632de808ccbf199af560dae0d73e5c833e
Noé Godinho <noe.godinho.92@gmail.com>
2016-09-18 13:47:18 +0100

    Code from exercise one to execute only on one processor

---
+++ Assignment_1/src/ex1.c
@@ -0,0 +1,20 @@
+#define _GNU_SOURCE
+#include <sched.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include "../lib/func.h"
+
+int main(){
+    cpu_set_t set;
+
+    /* clear cpu mask */
+    CPU_ZERO(&set);
+    /* set cpu 0 */ 
+    CPU_SET(0, &set);
+
+    /* 0 is the calling process */
+    if(sched_setaffinity(0, sizeof(cpu_set_t), &set) == -1){
+        perror("Error from sched_setaffinity");
+    }
+}
