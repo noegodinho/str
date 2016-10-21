86d318b54e177ce20d8e78a5e883a50c51a7b279
Noé Godinho <noe.godinho.92@gmail.com>
2016-10-21 16:56:00 +0100

    Optimized comparison

--- Assignment_1/src/ex5.c
+++ Assignment_1/src/ex5.c
@@ -7,37 +7,42 @@
 
 void calculate(double t, char *name);
 
+/* função que imita f1 de func.o */
 void f1(int n, int m){
     calculate(0.033, "f1");
 }
 
+/* função que imita f2 de func.o */
 void f2(int n, int m){
     calculate(0.053, "f2");
 }
 
+/* função que imita f3 de func.o */
 void f3(int n, int m){
     calculate(0.083, "f3");
 }
 
 void calculate(double t, char *name){
-    double t_atual;
+    double t_atual = 0;
     uint64_t t_nano = 0;
     struct timespec start, end;
 
-    while(1){
+    /* enquanto o tempo obtido até ao momento for inferior ao tempo pretendido */
+    while(t_atual < t){
+        /* começa o cálculo do tempo */
         clock_gettime(CLOCK_MONOTONIC, &start);
         
+        /* realiza umas iterações para ocupar CPU */
         for(int i = 0; i < 100000; ++i){
             ;
         }
 
+        /* termina o cálculo do tempo */ 
         clock_gettime(CLOCK_MONOTONIC, &end);
 
+        /* converte para nanosegundos os segundos e soma-os aos nanosegundos da estrutura */
         t_nano += (BILLION * (end.tv_sec - start.tv_sec) + end.tv_nsec - start.tv_nsec);
+        /* converte o obtido para segundos */
         t_atual = (double)t_nano / BILLION;
-
-        if(t_atual > t){
-            break;
-        }
     }
 }
